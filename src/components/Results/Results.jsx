ed6fd849705d2321c063c012043621f128025fe1
josevidmal <92562836+josevidmal@users.noreply.github.com>
2025-08-15 20:02:38 -0600

    Fix handleChange() function in App.jsx to make newValue a number and not a string for the <input> elements value. Add code to Results.jsx component to calculateInvestmentResults with userInput, compute totalInterest and invested capital, and render a table with Year, Investment Value, Interest (Year), Total Interest and Invested Capital.

--- src/components/Results/Results.jsx
+++ src/components/Results/Results.jsx
@@ -1,4 +1,31 @@
+import { calculateInvestmentResults, formatter } from '../../util/investment.js';
+
 export default function Results({ userInput }) {
-    console.log(userInput);
-    return <p>Results...</p>
-}
+    const dataArr = calculateInvestmentResults(userInput);
+    let totalInterest = 0;
+
+    return (
+        <table id="result">
+            <thead>
+                <tr>
+                    <th>Year</th>
+                    <th>Investment Value</th>
+                    <th>Interest (Year)</th>
+                    <th>Total Interest</th>
+                    <th>Invested Capital</th>
+                </tr>
+            </thead>
+            <tbody>
+                {dataArr.map(item => (
+                    <tr key={item.year}>
+                        <td>{item.year}</td>
+                        <td>{formatter.format(item.valueEndOfYear)}</td>
+                        <td>{formatter.format(item.interest)}</td>
+                        <td>{formatter.format(totalInterest += item.interest)}</td>
+                        <td>{formatter.format(item.valueEndOfYear - totalInterest)}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    );
+};
